d3ae215c85d932c61e021b3872cb88888f00355d
DragomirNikolov <dragomird.nikolov@gmail.com>
2024-10-28 14:10:36 +0100

    Added aggregatin capabilities
    There is a new flag to config_generation that gives the aggregation factor

--- Config_generator.py
+++ Config_generator.py
@@ -1,7 +1,3 @@
-#################################
-# Python script to create config files from within job so that the config files can use TMPDIR
-##################################
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -30,18 +26,20 @@ def parse_cmd_args():
     )
     parser.add_argument('-t',"--temperature_bounds" ,nargs=2, help="Min temp and max temp of file for analysis",type=int, required=True)
     parser.add_argument("-bc", "--base_configuration", help="Base yaml config file on which to draw upon", required=True)
+    parser.add_argument('-af',"--aggregation_factor" , help="Aggregation factor of file in use",type=int, required=False)
     # parser.add_argument("-wd", "--work_directory", help="Base yaml config file on which to draw upon", required=True)
     args = parser.parse_args()
 
     # Put arguments in a dictionary
     args_dict = {
         'temp_bounds': args.temperature_bounds,
-        'base_config': args.base_configuration
+        'base_config': args.base_configuration,
+        'agg_fact': args.aggregation_factor
     }
 
     return args_dict
 
-def config_setup(work_dir: str, min_temp, max_temp, base_setup_contnet: list) -> str:
+def config_setup(work_dir: str, min_temp, max_temp, base_setup_contnet: list, agg_fact=None) -> str:
     """
     Writes configuratuin file
 
@@ -56,14 +54,20 @@ def config_setup(work_dir: str, min_temp, max_temp, base_setup_contnet: list) ->
     """
     temp_setup = base_setup_contnet
     # Set temp_setup names
-    temp_setup["databasename"] = f"CTT-it-{round(abs(min_temp))}-{round(abs(max_temp))}-"
-    root_path=work_dir + f'/T-{round(abs(min_temp))}-{round(abs(max_temp))}'
+    if agg_fact!=None:
+        temp_setup["databasename"] = f"CTT-it-{round(abs(min_temp))}-{round(abs(max_temp))}-agg_{round(agg_fact)}-"
+        root_path=work_dir + f'/T-{round(abs(min_temp))}-{round(abs(max_temp))}-agg-{round(agg_fact)}'
+        print(root_path)
+    else:
+        temp_setup["databasename"] = f"CTT-it-{round(abs(min_temp))}-{round(abs(max_temp))}-"
+        root_path=work_dir + f'/T-{round(abs(min_temp))}-{round(abs(max_temp))}'
     #Create root path folder if it doesn't exist
     if not os.path.exists(root_path):
         os.makedirs(root_path)
     temp_setup['root_path'] = root_path
     temp_setup['logger_filepath'] = root_path+ '/log.log'
     temp_setup['error_filepath'] = root_path + '/err.log'
+    temp_setup['pixel_radius']= temp_setup['pixel_radius']*agg_fact
     temp_setup_fp = root_path + '/setup.yml'
     with open(temp_setup_fp, 'w') as temp_setup_file:
         yaml.dump(temp_setup, temp_setup_file)
@@ -72,14 +76,14 @@ def config_setup(work_dir: str, min_temp, max_temp, base_setup_contnet: list) ->
 if __name__=="__main__":
     base_dir = os.environ['TMPDIR']
     arg_dict=parse_cmd_args()
-
+    
     with open(arg_dict['base_config'], 'r') as stream:
         try:
             custom_setup = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
             exit
-    
-    config_setup(base_dir, abs(round(arg_dict['temp_bounds'][0])), abs(round(arg_dict['temp_bounds'][1])), custom_setup)
+    print("The agg factor is: ",arg_dict['agg_fact'])
+    config_setup(base_dir, abs(round(arg_dict['temp_bounds'][0])), abs(round(arg_dict['temp_bounds'][1])), custom_setup, agg_fact=arg_dict['agg_fact'])
     
 
