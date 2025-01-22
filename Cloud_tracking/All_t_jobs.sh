65ed8e69bf853d79e440fdc0a16397168cec5e4b
DragomirNikolov <dragomird.nikolov@gmail.com>
2025-01-22 14:32:06 +0100

    Update to postprocessing so it works with unresampled files

--- Cloud_tracking/All_t_jobs.sh
+++ Cloud_tracking/All_t_jobs.sh
@@ -6,7 +6,7 @@ for min_temp in $(seq $dt $dt 38); do
     max_temp=$((min_temp - dt))
     for pole in "np" "sp"; do
         name=T_"$min_temp"_"$max_temp"_"$agg_fact"_"$pole"
-        sbatch -J "$name" "$GTE_DIR"/Cloud_tracking/job.bsub -h $max_temp -l $min_temp -p $pole -c "/cluster/work/climate/dnikolo/n2o/Glaciation_time_estimator/config_w1.yaml"
+        sbatch -J "$name" "$GTE_DIR"/Cloud_tracking/job.bsub -h $max_temp -l $min_temp -p $pole -c "/cluster/work/climate/dnikolo/n2o/Glaciation_time_estimator/configs/config_testing_2024.yaml"
         #bash "$GTE_DIR"/Cloud_tracking/job.bsub  -h $max_temp -l $min_temp -a 3 -s $start_time -e $end_time -p $pole
     done
 done
