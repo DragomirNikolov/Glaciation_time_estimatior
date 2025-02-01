e6889c46c5bb773014caaed63dbe4c7ab477948b
DragomirNikolov <dragomird.nikolov@gmail.com>
2025-02-01 20:38:22 +0100

    Changes to configurations

--- Data_postprocessing/Copy_CLAAS_files.py
+++ Data_postprocessing/Copy_CLAAS_files.py
@@ -7,8 +7,6 @@ from Glaciation_time_estimator.Data_preprocessing.File_name_generator import gen
 from Glaciation_time_estimator.Auxiliary_func.config_reader import read_config
 from datetime import datetime
 import numpy as np
-import argparse
-
 
 def generate_remote_fps(pole):
     target_dict = generate_filename_dict(exclude_existing=False)
@@ -24,10 +22,8 @@ if __name__ == "__main__":
         target_folders =np.unique(vec_dirname(target_fps))
         data_dir= os.path.join(tmp_dir, "Data",pole, "")
         os.makedirs(data_dir, exist_ok=True)
-        print(target_folders)
-        print(data_dir)
-        print(config["postprocessing_output_dir"], config["CLAAS_fp"])
+        # print(target_folders)
+        # print(data_dir)
+        # print(config["postprocessing_output_dir"], config["CLAAS_fp"])
         for folder in target_folders:
             subprocess.run(["rsync", "-auq", f"{os.path.join(folder,'')}", data_dir ])
-
-    # subprocess.run("scp", )
